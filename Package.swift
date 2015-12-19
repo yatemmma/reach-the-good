7216d55b9a7873eeafa4a43bd62868689401061a
yatemmma <yatemmma@gmail.com>
2015-12-19 13:42:18 +0900

    wip

--- Package.swift
+++ Package.swift
@@ -4,5 +4,6 @@ let package = Package(
   name: "reach-the-tottori",
   dependencies: [
     .Package(url: "https://github.com/kylef/Curassow.git", majorVersion: 0, minor: 2),
+    .Package(url: "https://github.com/groue/GRMustache.swift", majorVersion: 1, minor: 0),
   ]
 )
--- Sources/main.swift
+++ Sources/main.swift
@@ -1,7 +1,41 @@
-import Glibc
+#if os(Linux)
+  import Glibc
+#endif
 import Inquiline
 import Curassow
+import Foundation
+import Mustache
 
-serve { _ in
-  return Response(.Ok, contentType: "text/plain", body: "Hello World")
+serve { request in
+  print("--------")
+  print(request.method)
+  print(request.path)
+  print(request.headers)
+  print(request.body)
+  
+  
+  if request.method == "GET" && request.path == "/" {
+  
+    let path = NSFileManager.defaultManager().currentDirectoryPath
+    let template = try! Template(path: "\(path)/Sources/index.mustache")
+
+    let data = [
+      "goodWord": "鳥取",
+      "badWord": "島根"
+    ]
+    
+    do {
+      let rendering = try template.render(Box(data))
+      print(rendering)
+      return Response(.Ok, contentType: "text/html; charset=UTF-8", body: rendering)
+    } catch let error as NSError {
+      print("something was wrong: \(error)")
+    }
+  }
+  
+  if request.method == "POST" && request.path == "/search" {
+    return Response(.Ok, contentType: "text/html; charset=UTF-8", body: "Hello World")
+  }
+  
+  return Response(.Ok, contentType: "text/html; charset=UTF-8", body: "なんじゃいこりゃ！長芋まんじゅうだがな              ")
 }
