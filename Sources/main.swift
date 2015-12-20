48d807d5cb05d8b53d5b1ed81b9eec9759d4e1d6
yatemmma <yatemmma@gmail.com>
2015-12-20 21:03:42 +0900

    wip

--- Sources/main.swift
+++ Sources/main.swift
@@ -6,43 +6,83 @@ import Curassow
 import Foundation
 import Mustache
 
+let GOOD_WORD = "鳥取県"
+let BAD_WORD  = "島根県"
+
 func genResponse(body: String) -> Response {
-  // バイト数 - 文字数 を足す？
-  return Response(.Ok, contentType: "text/html; charset=UTF-8", body: body)
+  var response = body
+  let num = body.utf8.count - body.characters.count
+  for _ in 1...num {
+    response = response + " "
+  }
+  return Response(.Ok, contentType: "text/html; charset=UTF-8", body: response)
+}
+
+func templatePath(name: String) -> String {
+  let current = NSFileManager.defaultManager().currentDirectoryPath
+  return "\(current)/Sources/\(name).mustache"
+}
+
+func dbpedia(keyword: String) -> [String] {
+  let urlString = "http://ja.dbpedia.org/data/\(keyword).json"
+  let encoded: String = urlString.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLQueryAllowedCharacterSet())!
+  let url: NSURL = NSURL(string: encoded)!
+  let response: NSData? = try! NSURLConnection.sendSynchronousRequest(NSURLRequest(URL: url), returningResponse: nil)
+  
+  let result: NSDictionary = try! NSJSONSerialization.JSONObjectWithData(response!, options: NSJSONReadingOptions.MutableContainers) as! NSDictionary
+  let rItems = result["http://ja.dbpedia.org/resource/\(keyword)"]!
+  let items: NSArray = rItems["http://dbpedia.org/ontology/wikiPageWikiLink"]! as! [NSDictionary]
+  
+  var links: [String] = []
+  for (_, item) in items.enumerate() {
+    let text = item["value"] as! String
+    links.append(text.stringByReplacingOccurrencesOfString("http://ja.dbpedia.org/resource/", withString: "", options: NSStringCompareOptions.LiteralSearch, range: nil))
+  }
+  return links
+}
+
+func status(items: [String]) -> String {
+  if items.contains(GOOD_WORD) && items.contains(BAD_WORD) {
+    return "even"
+  } else if items.contains(GOOD_WORD) {
+    return "good"
+  } else if items.contains(BAD_WORD) {
+    return "bad"
+  } else {
+    return "none"
+  }
 }
 
 serve { request in
   if request.method == "GET" && request.path == "/" {
   
-    let path = NSFileManager.defaultManager().currentDirectoryPath
-    let template = try! Template(path: "\(path)/Sources/index.mustache")
-
     let data = [
-      "goodWord": "鳥取県",
-      "badWord": "島根県"
+      "goodWord": GOOD_WORD,
+      "badWord": BAD_WORD
     ]
-    
-    let rendering = try! template.render(Box(data))
-    print(rendering)
-    return genResponse(rendering)
+
+    let template = try! Template(path: templatePath("index"))
+    return genResponse(try! template.render(Box(data)))
   }
   
   if request.method == "POST" && request.path == "/search" {
   
-    let path = NSFileManager.defaultManager().currentDirectoryPath
-    let template = try! Template(path: "\(path)/Sources/result.mustache")
-
+    let param: String? = request.body?.stringByRemovingPercentEncoding
+    let keywords: [String] = param!.componentsSeparatedByString("&").map({(x: String) -> String in
+      return x.componentsSeparatedByString("=")[1]
+    })
+  
+    let items = dbpedia(keywords[keywords.endIndex - 1])
     let data = [
-      "keywords": ["aaa", "vvv", "ccc"],
-      "items": ["aaaaaa", "鳥取県", "eeeee", "sssss"],
-      "status": "even",
-      "goodWord": "鳥取県",
-      "badWord": "島根県"
+      "keywords": keywords,
+      "items": items,
+      "status": status(items),
+      "goodWord": GOOD_WORD,
+      "badWord": BAD_WORD
     ]
     
-    let rendering = try! template.render(Box(data))
-    print(rendering)
-    return genResponse(rendering)
+    let template = try! Template(path: templatePath("result"))
+    return genResponse(try! template.render(Box(data)))
   }
   
   return genResponse("なんじゃいこりゃ！長芋まんじゅうだがな")
